2edcbc0b09cfa1e8e8d6bdcdd0914bef397a40ba
Bernardo Vieira <bernardo5@live.com.pt>
2020-06-26 13:11:17 +0100

    remove unused

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,13 +1,11 @@
 import { Sequelize } from 'sequelize';
 import { initializeCommunity } from './community';
 import { initializeTransactions } from './transactions';
-import { initializeBeneficiary } from './beneficiary';
 import { initializeUser } from './user';
 
 
 export default function initModels(sequelize: Sequelize) {
     initializeCommunity(sequelize);
     initializeTransactions(sequelize);
-    initializeBeneficiary(sequelize);
     initializeUser(sequelize);
 }
--- src/db/routes/index.ts
+++ src/db/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import community from './community';
 import transactions from './transactions';
-import beneficiary from './beneficiary';
 import user from './user';
 import s3 from './s3';
 
@@ -10,7 +9,6 @@ export default () => {
     const app = Router();
     community(app);
     transactions(app);
-    beneficiary(app);
     user(app);
     s3(app);
 
