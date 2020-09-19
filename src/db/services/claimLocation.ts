c1ec30a37783ab1e53b0776d1147973afb258eb2
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-19 23:53:55 +0100

    claim location + minimal typos

--- src/db/services/claimLocation.ts
+++ src/db/services/claimLocation.ts
@@ -3,9 +3,11 @@ import { ClaimLocation } from '../models/claimLocation';
 
 export default class ClaimLocationService {
     public static async add(
+        communityPublicId: string,
         gps: any,
     ): Promise<ClaimLocation> {
         return ClaimLocation.create({
+            communityPublicId,
             gps,
         });
     }
