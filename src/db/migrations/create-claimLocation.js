b762db0f703856ce4f922d22b835f102b40fcfbd
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-19 19:49:46 +0100

    new migration - globalStatus

--- src/db/migrations/create-claimLocation.js
+++ src/db/migrations/create-claimLocation.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('claimLocation', {
+        return queryInterface.createTable('claimlocation', {
             id: {
                 type: Sequelize.INTEGER,
                 autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
         });
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('claimLocation');
+        return queryInterface.dropTable('claimlocation');
     }
 };
