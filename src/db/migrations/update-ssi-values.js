1469f9a93a7aa47e6602f28c5310a7f395b4ca4e
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-28 15:25:27 +0100

    update ssi calculation

---
+++ src/db/migrations/update-ssi-values.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const math = require('mathjs');
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+
+        const m1 = [86700, 86400, 86400, 86400, 86400]
+        const n1 = [31, 26179, 3403, 3767, 1818]
+
+        const m2 = [86400, 87000, 86400, 86400, 86400, 86700, 86700, 86400]
+        const n2 = [164215, 3353, 26179, 1663, 3403, 3428, 430, 41293]
+
+        const m3 = [86700, 87000, 86400, 86700, 86700, 86700, 87000, 86400, 86400]
+        const n3 = [2841, 3353, 26179, 96, 30885, 3428, 252, 41293, 4518]
+
+        const m4 = [87000, 86400, 87300, 86400, 86700, 86700, 87000, 87300, 86400, 86400, 86700]
+        const n4 = [119, 166606, 499, 26179, 96, 30885, 71327, 5271, 41293, 67904, 580]
+
+        const calcSSI = (n, m) => parseFloat(((math.mad(n) / math.mean(m)) * 100 / 2).toFixed(2))
+        queryInterface.bulkUpdate('ssi', {
+            ssi: calcSSI(n1, m1),
+        }, {
+            id: 1,
+        });
+        queryInterface.bulkUpdate('ssi', {
+            ssi: calcSSI(n2, m2),
+        }, {
+            id: 2,
+        });
+        queryInterface.bulkUpdate('ssi', {
+            ssi: calcSSI(n3, m3),
+        }, {
+            id: 3,
+        });
+        queryInterface.bulkUpdate('ssi', {
+            ssi: calcSSI(n4, m4),
+        }, {
+            id: 4,
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+    }
+};
--- src/jobs/calculateSSI.ts
+++ src/jobs/calculateSSI.ts
@@ -3,6 +3,7 @@ import CommunityService from "../db/services/community";
 import TransactionsService from "../db/services/transactions";
 import { ethers } from "ethers";
 import SSIService from "../db/services/ssi";
+import math from 'mathjs';
 
 
 async function calcuateSSI(provider: ethers.providers.JsonRpcProvider): Promise<void> {
@@ -23,40 +24,19 @@ async function calcuateSSI(provider: ethers.providers.JsonRpcProvider): Promise<
             // the first time you don't wait a single second, the second time, only base interval
             const timeToWait = parseInt(community.vars._baseInterval, 10) + (communityClaims.get(beneficiryAdddress)! - 2) * parseInt(community.vars._incrementInterval, 10);
             const timeWaited = Math.floor((lastBeneficiaryClaim[0].txAt.getTime() - lastBeneficiaryClaim[1].txAt.getTime()) / 1000) - timeToWait;
-            console.log('timeWaited', timeWaited);
             beneficiariesTimeToWait.push(timeToWait);
             beneficiariesTimeWaited.push(timeWaited);
         }
-        // calculate mean
+        // calculate result
         console.log(beneficiariesTimeToWait, beneficiariesTimeWaited);
         if (beneficiariesTimeToWait.length > 1 && beneficiariesTimeWaited.length > 1) {
-            const meanTimeToWait = beneficiariesTimeToWait.reduce((a, b) => a + b, 0) / beneficiariesTimeToWait.length;
-            const meanTimeWaited = beneficiariesTimeWaited.reduce((a, b) => a + b, 0) / beneficiariesTimeWaited.length;
-            console.log(parseFloat(((meanTimeWaited / meanTimeToWait) * 100).toFixed(2)))
-            SSIService.add(community.publicId, new Date(), parseFloat(((meanTimeWaited / meanTimeToWait) * 100).toFixed(2)));
+            const meanTimeToWait = math.mean(beneficiariesTimeToWait);
+            const madTimeWaited = math.mad(beneficiariesTimeWaited);
+            SSIService.add(community.publicId, new Date(), parseFloat(((madTimeWaited / meanTimeToWait) * 50 /* aka, 100 / 2 */).toFixed(2)));
         }
     }
     // for each community
-    communities.forEach((community) => {
-        // // get date of the last ssi
-        // const lastSSI = await SSIService.last(community.publicId);
-        // // if there's no ssi, get the oldest claim
-        // if (lastSSI === undefined) {
-        //     // TODO: add option to calculate past ssi
-        //     const lastClaim = await TransactionsService.getLastClaim(community.contractAddress);
-        //     if (lastClaim === undefined) {
-        //         return;
-        //     }
-        //     // if older than 24h, it's necessary to calculate historical data. Get midnight that day, calculate and interate until the end
-        //     if (lastClaim.txAt.getTime() < new Date().getTime() - 86400000) {
-        //         // TODO: complete
-        //     }
-        //     // if not, don't do anything
-        // } else {
-
-        // }
-        calculateNewSSI(community);
-    });
+    communities.forEach((community) => calculateNewSSI(community));
 }
 
 
