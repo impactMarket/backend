8624ceb1eaebb530ece98b9eb1172b298a79add6
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-29 18:35:41 +0100

    comment out experimental

--- src/db/services/experimental.ts
+++ src/db/services/experimental.ts
@@ -1,27 +1,27 @@
-import IPFS from 'ipfs';
-import OrbitDB from 'orbit-db';
+// import IPFS from 'ipfs';
+// import OrbitDB from 'orbit-db';
 
-let db;
-const initOrbitDb = async () => {
-    const ipfs = await IPFS.create({
-        repo: './path-for-js-ipfs-repo',
-    });
-    const orbitdb = await OrbitDB.createInstance(ipfs);
-    // Create / Open a database
-    db = await orbitdb.docs('impactmarket.transactions');
-    await db.load();
-    console.log(db.id);
-};
-initOrbitDb();
+// let db;
+// const initOrbitDb = async () => {
+//     const ipfs = await IPFS.create({
+//         repo: './path-for-js-ipfs-repo',
+//     });
+//     const orbitdb = await OrbitDB.createInstance(ipfs);
+//     // Create / Open a database
+//     db = await orbitdb.docs('impactmarket.transactions');
+//     await db.load();
+//     console.log(db.id);
+// };
+// initOrbitDb();
 
 export default class ExperimentalService {
     public static async addTransaction(params: any): Promise<string> {
-        const hash = await db.put(params);
+        const hash = ''; //await db.put(params);
         return hash;
     }
 
     public static get(): any[] {
-        const result = db.get('');
+        const result = ['']; //db.get('');
         return result
     }
 }
