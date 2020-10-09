fefa13a53e12594081de03dc665b019fa15a9c88
Bernardo Vieira <bernardo5@live.com.pt>
2020-10-09 16:22:50 +0100

    order ssi

--- src/services/ssi.ts
+++ src/services/ssi.ts
@@ -17,7 +17,12 @@ export default class SSIService {
     public static async get(
         communityPublicId: string,
     ): Promise<{ dates: Date[], values: number[] }> {
-        const ssis = await SSI.findAll({ where: { communityPublicId }, attributes: ['date', 'ssi'], raw: true });
+        const ssis = await SSI.findAll({
+            where: { communityPublicId },
+            attributes: ['date', 'ssi'],
+            order: [['date', 'ASC']],
+            raw: true
+        });
         const dates = ssis.map((ssi) => ssi.date).slice(Math.max(0, ssis.length - 15), ssis.length);
         const values = ssis.map((ssi) => ssi.ssi).slice(Math.max(0, ssis.length - 15), ssis.length);
         return { dates, values };
