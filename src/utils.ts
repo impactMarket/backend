a1febeedb60b453fa2ad19aa18cd2570a03c6767
Bernardo Vieira <bernardo5@live.com.pt>
2020-10-22 14:29:18 +0100

    notifyBackersCommunityLowFunds method

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
+import { Expo, ExpoPushMessage } from 'expo-server-sdk';
 import axios from 'axios';
 import UserService from "./services/user";
 import Logger from './loaders/logger';
+import { ICommunityInfo } from './types';
 
 // Accepts the array and key
 export function groupBy<T>(array: any[], key: string): Map<string, T[]> {
@@ -12,6 +14,50 @@ export function groupBy<T>(array: any[], key: string): Map<string, T[]> {
         // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
         return result.set(currentValue[key], content);
     }, new Map<string, T[]>()); // empty map is the initial value for result object
+}
+
+export async function notifyBackersCommunityLowFunds(community: ICommunityInfo, backersPushTokens: string[]) {
+    // Create a new Expo SDK client
+    // optionally providing an access token if you have enabled push security
+    const expo = new Expo();
+    const basePushMessage: {
+        sound?: "default" | null | undefined;
+        title?: string | undefined;
+        body?: string | undefined;
+        data?: object | undefined;
+    } = {
+        sound: 'default',
+        title: 'A community you backed before is running out of funds.',
+        body: community.name + ' currently has less than 10% of funds available to be claimed by end beneficiaries. Back them again by sending cUSD (Celo Dollar) to their community contract.',
+        data: {
+            action: "community-low-funds",
+            communityAddress: community.contractAddress
+        },
+    }
+    // Create the messages that you want to send to clients
+    let messages: ExpoPushMessage[] = [];
+    for (let pushToken of backersPushTokens) {
+        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+        // Check that all your push tokens appear to be valid Expo push tokens
+        if (!Expo.isExpoPushToken(pushToken)) {
+            Logger.debug(`Push token ${pushToken} is not a valid Expo push token`);
+            continue;
+        }
+
+        // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+        messages.push({
+            to: pushToken,
+            ...basePushMessage
+        })
+    }
+
+    // The Expo push notification service accepts batches of notifications so
+    // that you don't need to send 1000 requests to send 1000 notifications. We
+    // recommend you batch your notifications to reduce the number of requests
+    // and to compress them (notifications with similar content will get
+    // compressed).
+    expo.chunkPushNotifications(messages);
 }
 
 export async function sendPushNotification(userAddress: string, title: string, body: string, data: any): Promise<boolean> {
