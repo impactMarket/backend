7df7d7004c08279afd1657f25d386e3f3aac1b54
Bernardo Vieira <bernardo5@live.com.pt>
2020-10-26 10:33:50 +0000

    manager model and service

---
+++ src/db/models/manager.ts
@@ -0,0 +1,51 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+
+export class Manager extends Model {
+    public id!: number;
+    public user!: string;
+    public communityId!: string;
+
+    // timestamps!
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
+export function initializeManager(sequelize: Sequelize): void {
+    return Manager.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        user: {
+            type: DataTypes.STRING(44),
+            references: {
+                model: 'user', // name of Target model
+                key: 'address', // key in Target model that we're referencing
+            },
+            onDelete: 'RESTRICT',
+            allowNull: false,
+        },
+        communityId: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'community', // name of Target model
+                key: 'publicId', // key in Target model that we're referencing
+            },
+            onDelete: 'RESTRICT',
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        }
+    }, {
+        tableName: 'manager',
+        sequelize: sequelize, // this bit is important
+    });
+}
---
+++ src/services/managers.ts
@@ -0,0 +1,36 @@
+import { Manager } from '../db/models/manager';
+
+
+export default class ManagerService {
+
+    public static async add(
+        address: string,
+        communityId: string
+    ): Promise<boolean> {
+        // if user does not exist, add to pending list
+        // otherwise update
+        const user = await Manager.findOne({ where: { user: address, communityId } });
+        if (user === null) {
+            const updated = await Manager.create({
+                user: address,
+                communityId
+            });
+            return updated[0] > 0;
+        }
+        return true;
+    }
+
+
+    public static async get(
+        address: string,
+    ): Promise<Manager | null> {
+        return await Manager.findOne({ where: { user: address } });
+    }
+
+    public static async remove(
+        address: string,
+        communityId: string
+    ): Promise<void> {
+        await Manager.destroy({ where: { user: address, communityId } });
+    }
+}
