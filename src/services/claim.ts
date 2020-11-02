7a5f14f7aaf4eb7b928eed7db23ed47a8453f624
Bernardo Vieira <bernardo5@live.com.pt>
2020-11-02 11:02:36 +0000

    get monthly claimed and raised

--- src/services/claim.ts
+++ src/services/claim.ts
@@ -1,8 +1,9 @@
+import { fn, Op } from 'sequelize';
 import { Claim } from '../db/models/claim';
 import Logger from '../loaders/logger';
 
 
-export default class ClaimsService {
+export default class ClaimService {
 
     public static async add(
         address: string,
@@ -18,8 +19,34 @@ export default class ClaimsService {
                 tx,
                 txAt: new Date(),
             });
-        } catch(e) {
+        } catch (e) {
             Logger.info(e);
         }
+    }
+
+    /**
+     * Get monthly (last 30 days) claimed amounts grouped by community.
+     * 
+     * **NOTE**: claimed amounts will always be bigger than zero though,
+     * a community might not be listed if no claim has ever happened!
+     * 
+     * @returns Map< communityId, amount >
+     */
+    public static async getMonthlyClaimed(): Promise<Map<string, string>> {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const aMonthAgo = new Date(today.getTime() - 2592000000); // 30 * 24 * 60 * 60 * 1000
+        const claimed = await Claim.findAll({
+            attributes: ['communityId', [fn('sum', 'amount'), 'claimed']],
+            where: {
+                txAt: {
+                    [Op.lt]: today,
+                    [Op.gte]: aMonthAgo,
+                }
+            },
+            group: 'communityId',
+        });
+        // there will always be claimed.lenght > 0 (were only zero at the begining)
+        return new Map((claimed as any).map((c: { claimed: string, communityId:string }) => [c.communityId, c.claimed]));
     }
 }
--- src/services/inflow.ts
+++ src/services/inflow.ts
@@ -1,3 +1,4 @@
+import { fn, Op } from 'sequelize';
 import { Inflow } from '../db/models/inflow';
 import Logger from '../loaders/logger';
 
@@ -21,5 +22,31 @@ export default class InflowService {
         } catch(e) {
             Logger.info(e);
         }
+    }
+
+    /**
+     * Get monthly (last 30 days) raised amounts grouped by community.
+     * 
+     * **NOTE**: raised amounts will always be bigger than zero though,
+     * a community might not be listed if no raise has ever happened!
+     * 
+     * @returns Map< communityId, amount >
+     */
+    public static async getMonthlyRaised(): Promise<Map<string, string>> {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const aMonthAgo = new Date(today.getTime() - 2592000000); // 30 * 24 * 60 * 60 * 1000
+        const raised = await Inflow.findAll({
+            attributes: ['communityId', [fn('sum', 'amount'), 'raised']],
+            where: {
+                txAt: {
+                    [Op.lt]: today,
+                    [Op.gte]: aMonthAgo,
+                }
+            },
+            group: 'communityId',
+        });
+        // there will always be raised.lenght > 0 (were only zero at the begining)
+        return new Map((raised as any).map((c: { raised: string, communityId:string }) => [c.communityId, c.raised]));
     }
 }
