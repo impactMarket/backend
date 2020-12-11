e415916c0529aa03990c2f04641c37ac6ae03384
Bernardo Vieira <bernardo5@live.com.pt>
2020-12-11 22:19:38 +0000

    remove unused experimental

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import claimLocation from './claimLocation';
 import community from './community';
 import exchange from './exchangeRates';
-import experimental from './experimental';
 import globalStatus from './globalStatus';
 import mobile from './mobile';
 import s3 from './s3';
@@ -19,7 +18,6 @@ export default (): Router => {
     s3(app);
     claimLocation(app);
     exchange(app);
-    experimental(app);
     globalStatus(app);
     mobile(app);
     storage(app);
