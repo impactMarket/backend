850ba6de1cb940b118f4a67389c7d95bf57b2717
Bernardo Vieira <bernardo@impactmarket.com>
2021-01-19 16:11:13 +0000

    change aws config

--- src/api/routes/storage.ts
+++ src/api/routes/storage.ts
@@ -3,7 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import { Logger } from '@logger/logger';
 import config from '../../config';
-import { S3 } from 'aws-sdk';
+import AWS from 'aws-sdk';
 import fleekStorage from '@fleekhq/fleek-storage-js';
 import CommunityService from '@services/community';
 import sharp from 'sharp';
@@ -13,13 +13,15 @@ export default (app: Router): void => {
     const route = Router();
     const storage = multer.memoryStorage();
     const upload = multer({ storage: storage });
-    new S3({
+    console.log('1');
+    new AWS.Config({
         accessKeyId: config.aws.accessKeyId,
         secretAccessKey: config.aws.secretAccessKey,
         region: config.aws.region,
         //
         apiVersion: "2006-03-01",
     });
+    console.log('2');
 
     app.use('/storage', route);
 
@@ -27,7 +29,9 @@ export default (app: Router): void => {
         '/upload',
         (req, res) => {
             try {
+                console.log('reading');
                 upload.single('imageFile')(req, res, async (err) => {
+                    console.log('starting');
                     if (err) {
                         Logger.error('Error during /upload ', err);
                     }
@@ -37,6 +41,7 @@ export default (app: Router): void => {
                             chromaSubsampling: '4:4:4'
                         })
                         .toBuffer();
+                    console.log('formatted');
                     const { communityId } = req.body;
                     const fileExtension = path.extname(req.file.originalname);
                     const today = new Date();
@@ -44,14 +49,14 @@ export default (app: Router): void => {
                     const s3Filename = `${Date.now().toString()}${fileExtension}`;
                     const s3FilePath = `${s3FilePrefix}${s3Filename}`;
                     try {
-                        const params: S3.PutObjectRequest = {
+                        const params: AWS.S3.PutObjectRequest = {
                             Bucket: config.aws.picturesBucket,
                             Key: s3FilePath,
                             Body: imgBuffer,
                             ACL: 'public-read'
                         };
                         //
-                        const upload = new S3.ManagedUpload({ params });
+                        const upload = new AWS.S3.ManagedUpload({ params });
                         const uploadResult = await upload.promise();
                         console.log(uploadResult)
                         // update community picture
