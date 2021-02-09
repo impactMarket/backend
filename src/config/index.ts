a2326242212f0967f44fe56434986dd6ac9b1f40
Bernardo Vieira <bernardo@impactmarket.com>
2021-02-09 14:12:54 +0000

    config for tests

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv';
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'development') {
+    const envFound = dotenv.config();
+    if (!envFound) {
+        // This error should crash whole process
 
-const envFound = dotenv.config();
-if (!envFound) {
-    // This error should crash whole process
+        throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    }
+} else if (process.env.NODE_ENV === 'test') {
+    const envFound = dotenv.config({ path: '.env.test' });
+    if (!envFound) {
+        // This error should crash whole process
 
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+        throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    }
 }
 
 export default {
