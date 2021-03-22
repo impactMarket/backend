dd5c1140ff9a898b7073a6ed046b666182743870
Bernardo Vieira <bernardo@impactmarket.com>
2021-03-22 17:13:17 +0000

    create trust profile to user

--- src/api/validators/user.ts
+++ src/api/validators/user.ts
@@ -11,64 +11,65 @@ const authenticate = celebrate({
     body: Joi.object({
         address: Joi.string().required(),
         language: Joi.string().required(),
-        currency: Joi.string().optional().allow(''),
+        currency: Joi.string().required(),
         pushNotificationToken: Joi.string().required().allow(''),
-        phone: Joi.string().optional(),
+        phone: Joi.string().optional(), // TODO: make it required once 1.0.7 is the minimal mobile version!
     }),
 });
 
 const hello = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         token: Joi.string().allow(''),
+        phone: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
     }),
 });
 
 const updateUsername = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         username: Joi.string().required().allow(''),
     }),
 });
 
 const updateCurrency = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         currency: Joi.string().required(),
     }),
 });
 
 const updatePushNotificationsToken = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         token: Joi.string().required(),
     }),
 });
 
 const updateLanguage = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         language: Joi.string().required(),
     }),
 });
 
 const updateGender = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         gender: Joi.string().required().allow(''),
     }),
 });
 
 const updateAge = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         age: Joi.number().required().allow(null),
     }),
 });
 
 const updateChildren = celebrate({
     body: Joi.object({
-        address: Joi.string().required(),
+        address: Joi.string().optional(), // TODO: remove it once 1.0.7 is the minimal mobile version!
         children: Joi.number().required().allow(null),
     }),
 });
