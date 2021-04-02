d69610a0f9002897e0012ac541b7761b76d984bd
Bernardo Vieira <bernardo@impactmarket.com>
2021-04-02 15:59:02 +0100

    new env vars + refactor config

--- src/config/validatenv.ts
+++ src/config/validatenv.ts
@@ -17,38 +17,46 @@ if (process.env.NODE_ENV === 'development') {
     }
 }
 
+function onlyOnTestEnv(testDefault: any) {
+    return process.env.NODE_ENV === 'test' ? testDefault : undefined;
+}
+
 function validateEnv() {
-    console.log(process.env.NODE_ENV);
     return cleanEnv(process.env, {
         NODE_ENV: str(),
-        PORT: port({ devDefault: 500 }),
-        DATABASE_URL: str({ devDefault: 'xyz' }),
-        CHAIN_JSON_RPC_URL: str({ devDefault: 'xyz' }),
-        JWT_SECRET: str({ devDefault: 'xyz' }),
-        CUSD_CONTRACT_ADDRESS: str({ devDefault: 'xyz' }),
-        IMPACT_MARKET_CONTRACT_ADDRESS: str({ devDefault: 'xyz' }),
+        PORT: port({ devDefault: onlyOnTestEnv(5000) }),
+        DATABASE_URL: str(),
+        CHAIN_JSON_RPC_URL: str({ devDefault: onlyOnTestEnv('xyz') }),
+        JWT_SECRET: str({ devDefault: onlyOnTestEnv('xyz') }),
+        CUSD_CONTRACT_ADDRESS: str({ devDefault: onlyOnTestEnv('xyz') }),
+        IMPACT_MARKET_CONTRACT_ADDRESS: str({
+            devDefault: onlyOnTestEnv('xyz'),
+        }),
         IMPACT_MARKET_CONTRACT_BLOCK: num({ devDefault: 0 }),
-        ATTESTATION_PROXY_ADDRESS: str({ devDefault: 'xyz' }),
-        AWS_ACCESS_KEY_ID: str({ devDefault: 'xyz' }),
-        AWS_SECRET_ACCESS_KEY: str({ devDefault: 'xyz' }),
-        AWS_BUCKET_IMAGES_COMMUNITY: str({ devDefault: 'xyz' }),
-        AWS_BUCKET_IMAGES_STORY: str({ devDefault: 'xyz' }),
-        AWS_REGION: str({ devDefault: 'xyz' }),
-        CURRENCIES_API_BASE_URL: str({ devDefault: 'xyz' }),
-        CURRENCIES_API_KEY: str({ devDefault: 'xyz' }),
-        MASTER_KEY: str({ devDefault: 'xyz' }),
-        LATEST_MOBILE_APP_VERSION: str({ devDefault: 'xyz' }),
-        MINIMAL_MOBILE_APP_VERSION: str({ devDefault: 'xyz' }),
+        ATTESTATION_PROXY_ADDRESS: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_ACCESS_KEY_ID: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_SECRET_ACCESS_KEY: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_BUCKET_COMMUNITY_COVER: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_BUCKET_COMMUNITY_LOGO: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_BUCKET_IMAGES_STORY: str({ devDefault: onlyOnTestEnv('xyz') }),
+        AWS_REGION: str({ devDefault: onlyOnTestEnv('xyz') }),
+        CURRENCIES_API_BASE_URL: str({ devDefault: onlyOnTestEnv('xyz') }),
+        CURRENCIES_API_KEY: str({ devDefault: onlyOnTestEnv('xyz') }),
+        MASTER_KEY: str({ devDefault: onlyOnTestEnv('xyz') }),
+        LATEST_MOBILE_APP_VERSION: str({ devDefault: onlyOnTestEnv('0.0.0') }),
+        MINIMAL_MOBILE_APP_VERSION: str({ devDefault: onlyOnTestEnv('0.0.1') }),
         LOG_LEVEL: str({ default: 'verbose' }),
-        FLEEK_STORAGE_KEY_ID: str({ devDefault: 'xyz' }),
-        FLEEK_STORAGE_ACCESS_KEY: str({ devDefault: 'xyz' }),
-        COMMUNITY_PLACEHOLDER_IMAGE_URL: str({ devDefault: 'xyz' }),
+        FLEEK_STORAGE_KEY_ID: str({ devDefault: onlyOnTestEnv('xyz') }),
+        FLEEK_STORAGE_ACCESS_KEY: str({ devDefault: onlyOnTestEnv('xyz') }),
+        COMMUNITY_PLACEHOLDER_IMAGE_URL: str({
+            devDefault: onlyOnTestEnv('xyz'),
+        }),
         API_ENVIRONMENT: str({
             choices: ['development', 'test', 'production', 'staging'],
             devDefault: 'test',
         }),
-        CLOUDFRONT_URL: str({ devDefault: 'xyz' }),
-        SENTRY_KEY: str({ devDefault: 'xyz' }),
+        CLOUDFRONT_URL: str({ devDefault: onlyOnTestEnv('xyz') }),
+        SENTRY_KEY: str({ devDefault: onlyOnTestEnv('xyz') }),
         SENTRY_TRACE_SAMPLES_RATE: num({ default: 0 }),
         ADMIN_KEY: str({ devDefault: 'xyz' }),
         HASH_KEY: str({ devDefault: 'xyz' }),
