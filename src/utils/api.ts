535717364446b0b476777c2252d7f4513cefb290
Bernardo Vieira <bernardo@impactmarket.com>
2021-04-20 15:39:04 +0100

    standardResponse experimental for API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,3 +5,19 @@ export const controllerLogAndFail = (e: any, status: number, res: Response) => {
     Logger.error(e);
     res.status(status).send(e);
 };
+
+export const standardResponse = (
+    res: Response,
+    status: number,
+    success: boolean,
+    data: any,
+    errorMessage?: string,
+    count?: number
+) => {
+    res.status(status).send({
+        success,
+        data,
+        errorMessage,
+        count,
+    });
+};
