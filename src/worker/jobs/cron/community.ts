1141993abc006db056c6e3baa459c621e9e81fe3
Bernardo Vieira <bernardo@impactmarket.com>
2021-04-29 12:56:55 +0100

    fix replacemente issues

--- src/worker/jobs/cron/community.ts
+++ src/worker/jobs/cron/community.ts
@@ -182,7 +182,7 @@ export async function calcuateCommunitiesMetrics(): Promise<void> {
         reach: string;
         id: string;
     }[] = await sequelize.query(
-        'select sum(bt.amount) volume, count(bt.amount) txs, count(distinct bt."withAddress") reach, c.id from beneficiarytransaction bt left join beneficiary b on bt.beneficiary = b.address left join community c on b."communityId" = c."publicId" where bt.date = ? group by c.id',
+        'select sum(bt.amount) volume, count(bt.amount) txs, count(distinct bt."withAddress") reach, c.id from beneficiarytransaction bt left join beneficiary b on bt.beneficiary = b.address left join community c on b."communityId" = c."publicId" where bt.date = \'?\' group by c.id',
         {
             replacements: [yesterday.toISOString().split('T')[0]],
             raw: true,
