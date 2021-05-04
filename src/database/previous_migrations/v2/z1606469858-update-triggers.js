c846578d3539feb8973f932793af18b1e87c3867
Bernardo Vieira <bernardo@impactmarket.com>
2021-05-04 10:55:29 +0100

    fix trigger migrations

--- src/database/previous_migrations/v2/z1606469858-update-triggers.js
+++ src/database/previous_migrations/v2/z1606469858-update-triggers.js
@@ -7,30 +7,33 @@ module.exports = {
 
         // create trigger to update total beneficiaries ina  given community, by day and since ever
         await queryInterface.sequelize.query(`
+        DROP TRIGGER update_beneficiaries_community_states ON beneficiary;
+        
         CREATE OR REPLACE FUNCTION update_beneficiaries_community_states()
     RETURNS TRIGGER AS $$
+    declare
+        community_id integer;
     BEGIN
-        IF (TG_OP = 'INSERT') THEN -- INSERT operations
+        SELECT id INTO community_id FROM community where "publicId"=NEW."communityId";
+        IF (TG_OP = 'INSERT') THEN -- INSERT operations (first added)
             -- update overall state
-            UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=NEW."communityId";
+            UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id;
             -- update daily state
-            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=NEW."communityId" AND date=DATE(NEW."txAt");
-        ELSEIF (OLD.active IS FALSE AND NEW.active IS TRUE) THEN
+            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id AND date=DATE(NEW."txAt");
+        ELSEIF (OLD.active IS FALSE AND NEW.active IS TRUE) THEN -- beneficiary being added back to community
             -- update overall state
-            UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=NEW."communityId";
+            UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1, "removedBeneficiaries" = "removedBeneficiaries" - 1 WHERE "communityId"=community_id;
             -- update daily state
-            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=NEW."communityId" AND date=DATE(NEW."txAt");
-        ELSEIF (OLD.active IS TRUE AND NEW.active IS FALSE) THEN
+            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id AND date=DATE(NEW."txAt");
+        ELSEIF (OLD.active IS TRUE AND NEW.active IS FALSE) THEN -- beneficiary being removed from community
             -- update overall state
-            UPDATE ubi_community_state SET beneficiaries = beneficiaries - 1 WHERE "communityId"=NEW."communityId";
+            UPDATE ubi_community_state SET beneficiaries = beneficiaries - 1, "removedBeneficiaries" = "removedBeneficiaries" + 1 WHERE "communityId"=community_id;
             -- update daily state
-            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries - 1 WHERE "communityId"=NEW."communityId" AND date=DATE(NEW."txAt");
+            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries - 1 WHERE "communityId"=community_id AND date=DATE(NEW."txAt");
         END IF;
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
-
-DROP TRIGGER update_beneficiaries_community_states ON beneficiary;
 
 CREATE TRIGGER update_beneficiaries_community_states
 AFTER INSERT OR UPDATE
