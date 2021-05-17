158ad965d9f1d7e38dea301ed5474518f0240084
Bernardo Vieira <bernardo@impactmarket.com>
2021-05-17 10:53:40 +0100

    clean up trigger

--- src/database/migrations/zz-create-triggers.js
+++ src/database/migrations/zz-create-triggers.js
@@ -13,14 +13,9 @@ module.exports = {
         community_id integer;
     BEGIN
         SELECT id INTO community_id FROM community where "publicId"=NEW."communityId";
-        IF (TG_OP = 'INSERT') THEN -- INSERT operations (first added)
+        IF (TG_OP = 'INSERT') THEN -- INSERT operations (beneficiary being added from community)
             -- update overall state
             UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id;
-            -- update daily state
-            UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id AND date=DATE(NEW."txAt");
-        ELSEIF (OLD.active IS FALSE AND NEW.active IS TRUE) THEN -- beneficiary being added back to community
-            -- update overall state
-            UPDATE ubi_community_state SET beneficiaries = beneficiaries + 1, "removedBeneficiaries" = "removedBeneficiaries" - 1 WHERE "communityId"=community_id;
             -- update daily state
             UPDATE ubi_community_daily_state SET beneficiaries = beneficiaries + 1 WHERE "communityId"=community_id AND date=DATE(NEW."txAt");
         ELSEIF (OLD.active IS TRUE AND NEW.active IS FALSE) THEN -- beneficiary being removed from community
