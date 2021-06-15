78db600386e8c2e76c6ebeb2d8430f770308c5df
Bernardo Vieira <bernardo@impactmarket.com>
2021-06-15 08:37:27 +0100

    refactor app init to support tests

--- src/api/app.ts
+++ src/api/app.ts
@@ -8,45 +8,47 @@ import config from '../config';
 import { sequelize } from '../database';
 import serverLoader from './server';
 
-async function startServer() {
+export async function startServer() {
     const app = express();
 
-    Sentry.init({
-        dsn: config.sentryKey,
-        debug: process.env.NODE_ENV === 'development',
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({ tracing: true }),
-            // enable Express.js middleware tracing
-            new Integrations.Express({ app }),
-        ],
-        tracesSampler: (samplingContext) => {
-            // Examine provided context data (including parent decision, if any) along
-            // with anything in the global namespace to compute the sample rate or
-            // sampling decision for this transaction
+    if (process.env.NODE_ENV !== 'test') {
+        Sentry.init({
+            dsn: config.sentryKey,
+            debug: process.env.NODE_ENV === 'development',
+            integrations: [
+                // enable HTTP calls tracing
+                new Sentry.Integrations.Http({ tracing: true }),
+                // enable Express.js middleware tracing
+                new Integrations.Express({ app }),
+            ],
+            tracesSampler: (samplingContext) => {
+                // Examine provided context data (including parent decision, if any) along
+                // with anything in the global namespace to compute the sample rate or
+                // sampling decision for this transaction
 
-            // always inherit
-            if (samplingContext.parentSampled !== undefined) {
-                return samplingContext.parentSampled;
-            }
+                // always inherit
+                if (samplingContext.parentSampled !== undefined) {
+                    return samplingContext.parentSampled;
+                }
 
-            if (
-                samplingContext.transactionContext.name.indexOf(
-                    '/mobile/error'
-                ) !== -1 ||
-                samplingContext.transactionContext.name.indexOf(
-                    '/mobile/version'
-                ) !== -1
-            ) {
-                // Ignore this.
-                return 0;
-            } else {
-                // Default sample rate
-                return config.tracesSampleRate;
-            }
-        },
-        tracesSampleRate: config.tracesSampleRate,
-    });
+                if (
+                    samplingContext.transactionContext.name.indexOf(
+                        '/mobile/error'
+                    ) !== -1 ||
+                    samplingContext.transactionContext.name.indexOf(
+                        '/mobile/version'
+                    ) !== -1
+                ) {
+                    // Ignore this.
+                    return 0;
+                } else {
+                    // Default sample rate
+                    return config.tracesSampleRate;
+                }
+            },
+            tracesSampleRate: config.tracesSampleRate,
+        });
+    }
 
     if (process.env.NODE_ENV === 'development') {
         Logger.debug('DEBUG');
@@ -57,13 +59,15 @@ async function startServer() {
     }
 
     await sequelize.authenticate();
-    await sequelize.sync();
+    if (process.env.NODE_ENV !== 'test') {
+        await sequelize.sync();
+    }
     Logger.info('🗺️  Database loaded and connected');
 
     await serverLoader(app);
     Logger.info('📡 Express server loaded');
 
-    app.listen(config.port, () => {
+    return app.listen(config.port, () => {
         Logger.info(`
         ################################################
         🛡️  Server listening on port: ${config.port} 🛡️ 
@@ -72,4 +76,6 @@ async function startServer() {
     });
 }
 
-startServer();
+if (process.env.NODE_ENV !== 'test') {
+    startServer();
+}
