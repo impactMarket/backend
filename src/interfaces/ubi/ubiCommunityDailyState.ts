90ec190ac5eb7eb6fef7bdbadadb66744e5db281
Bernardo Vieira <bernardo@impactmarket.com>
2021-06-22 14:47:59 +0100

    refactor UbiCommunityDailyStateCreation

--- src/interfaces/ubi/ubiCommunityDailyState.ts
+++ src/interfaces/ubi/ubiCommunityDailyState.ts
@@ -16,5 +16,16 @@ export interface UbiCommunityDailyState {
 }
 export interface UbiCommunityDailyStateCreation {
     communityId: number;
+    claimed: string;
+    claims: number;
+    beneficiaries: number;
+    raised: string;
+    backers: number;
+    monthlyBackers: number;
+    volume: string;
+    transactions: number;
+    reach: number;
+    reachOut: number;
+    fundingRate: number;
     date: Date;
 }
