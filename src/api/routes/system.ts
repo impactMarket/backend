131973686a681d09653917ee87f89cccac08d7d6
Bernardo Vieira <bernardo@impactmarket.com>
2021-06-22 14:47:45 +0100

    remove unused

--- src/api/routes/system.ts
+++ src/api/routes/system.ts
@@ -22,17 +22,4 @@ export default (app: Router): void => {
             .then(() => res.sendStatus(200))
             .catch(() => res.sendStatus(400));
     });
-
-    /**
-     * for backwards compatibility, remove ASAP
-     */
-    app.get('/global-status', async (req, res) => {
-        res.send({
-            monthly: await globalDailyStateService.getLast30Days(),
-            lastQuarterAvgSSI: await globalDailyStateService.last90DaysAvgSSI(),
-            today: await CommunityDailyStateService.notYetCountedToday(),
-            totalBackers: await InflowService.countEvergreenBackers(),
-            reachedLastMonth: await reachedAddressService.getAllReachedLast30Days(),
-        });
-    });
 };
