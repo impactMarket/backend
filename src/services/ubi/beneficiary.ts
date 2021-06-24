557df8d4768ad52ce29b0967ab6db6d5dcbecf02
Bernardo Vieira <bernardo@impactmarket.com>
2021-06-24 17:36:34 +0100

    remove unused

--- src/services/ubi/beneficiary.ts
+++ src/services/ubi/beneficiary.ts
@@ -4,7 +4,7 @@ import { Beneficiary, BeneficiaryAttributes } from '@models/ubi/beneficiary';
 import { ManagerAttributes } from '@models/ubi/manager';
 import { Logger } from '@utils/logger';
 import { isAddress } from '@utils/util';
-import { Op, fn, col, WhereAttributeHash, literal } from 'sequelize';
+import { Op, WhereAttributeHash, literal } from 'sequelize';
 import { Literal, Where } from 'sequelize/types/lib/utils';
 
 import { models, sequelize } from '../../database';
@@ -147,22 +147,7 @@ export default class BeneficiaryService {
         return result;
     }
 
-    public static listActiveInCommunity(
-        communityId: string
-    ): Promise<
-        { claims: number; lastClaimAt: Date; penultimateClaimAt: Date }[]
-    > {
-        return this.beneficiary.findAll({
-            attributes: ['claims', 'lastClaimAt', 'penultimateClaimAt'],
-            where: {
-                communityId,
-                active: true,
-            },
-            raw: true,
-        }) as any;
-    }
-
-    public static async listBeneficiaries(
+    public static async list(
         managerAddress: string,
         active: boolean,
         offset: number,
@@ -253,26 +238,5 @@ export default class BeneficiaryService {
                 raw: true,
             })
         ).map((b) => b.address);
-    }
-
-    public static async getActiveBeneficiariesLast30Days(): Promise<
-        Map<string, number>
-    > {
-        const todayMidnightTime = new Date();
-        todayMidnightTime.setHours(0, 0, 0, 0);
-        // a month ago, from todayMidnightTime
-        const aMonthAgo = new Date(todayMidnightTime.getTime() - 2592000000); // 30 * 24 * 60 * 60 * 1000
-        const result = await this.beneficiary.findAll({
-            attributes: [[fn('count', col('address')), 'total'], 'communityId'],
-            where: {
-                lastClaimAt: {
-                    [Op.lt]: todayMidnightTime,
-                    [Op.gte]: aMonthAgo,
-                },
-            },
-            group: 'communityId',
-            raw: true,
-        });
-        return new Map(result.map((c: any) => [c.communityId, c.total]));
     }
 }
