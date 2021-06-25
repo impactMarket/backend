203aba1e5b329ada42ebb26553d77022fc299dc6
Bernardo Vieira <bernardo@impactmarket.com>
2021-06-25 15:12:18 +0100

    add migration

--- src/database/migrations/z1624461148-update-user.js
+++ src/database/migrations/z1624461148-update-user.js
@@ -6,7 +6,19 @@ module.exports = {
         if (process.env.NODE_ENV === 'test') {
             return;
         }
-        //
+        const query =
+            'select "user".address from "user" left join app_user_through_trust autt on "user".address = autt."userAddress" left join app_user_trust aut on autt."appUserTrustId" = aut.id where suspect = true';
+
+        const suspectUsers = await queryInterface.sequelize.query(query, {
+            raw: true,
+            type: Sequelize.QueryTypes.SELECT,
+        });
+        for (const user in suspectUsers) {
+            const element = suspectUsers[user];
+            await queryInterface.sequelize.query(
+                `update "user" set suspect=true where address=${element.address}`
+            );
+        }
     },
 
     down(queryInterface, Sequelize) {},
