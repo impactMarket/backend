9f25810fcf5ead2791a6680e9dbe886ccb34872f
Bernardo Vieira <bernardo@impactmarket.com>
2021-07-02 11:31:18 +0100

    return only updated media updateMedia

--- src/services/media.ts
+++ src/services/media.ts
@@ -4,14 +4,18 @@ import { AppMediaThumbnailCreation } from '@interfaces/app/appMediaThumbnail';
 import { models } from '../database';
 
 export class MediaService {
-    updateMedia(content: AppMediaContentCreation) {
-        return models.appMediaContent.update(
+    async updateMedia(content: AppMediaContentCreation) {
+        const r = await models.appMediaContent.update(
             {
                 height: content.height,
                 width: content.width,
             },
-            { where: { url: content.url } }
+            { where: { url: content.url }, returning: true }
         );
+        if (r[0] > 0) {
+            return r[1][0];
+        }
+        throw new Error('not updated!');
     }
 
     postThumbnail(content: AppMediaThumbnailCreation) {
