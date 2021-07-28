8a6c70faad44cf10d42a57bcfbec5011b3f842ec
Bernardo Vieira <bernardo@impactmarket.com>
2021-07-28 12:45:49 +0100

    une API_ENVIRONMENT only as condition in release

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npx sequelize db:migrate --env $API_ENVIRONMENT
-if [ $API_ENVIRONMENT != 'production' ] && [ $NODE_ENV != 'production' ] && [ $NODE_ENV != 'prod' ] && [ $NODE_ENV != 'PROD' ]
+if [ $API_ENVIRONMENT != 'production' ]
 then
     npx sequelize-cli db:seed:all --env $API_ENVIRONMENT
 fi
