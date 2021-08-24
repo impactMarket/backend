2b32e518e8a5a05a99d1d73d74183310a64f82e3
João Pedro <joao@impactmarket.com>
2021-08-24 15:43:17 -0300

    fix with query

--- src/worker/jobs/cron/user.ts
+++ src/worker/jobs/cron/user.ts
@@ -6,7 +6,7 @@ export async function verifyUserSuspectActivity(): Promise<void> {
     const query = `WITH 
     active_users 
         AS (SELECT "user".address, "trust".phone
-                FROM public.user AS "user" 
+                FROM "user" 
                 LEFT OUTER JOIN app_user_through_trust AS "through_trust"
                     ON "user".address = "through_trust"."userAddress"
                 LEFT OUTER JOIN app_user_trust AS "trust"
@@ -16,8 +16,8 @@ export async function verifyUserSuspectActivity(): Promise<void> {
         AS (SELECT address FROM (SELECT phone FROM active_users GROUP BY phone HAVING count(phone) > 1) as phones 
                 LEFT OUTER JOIN active_users ON active_users.phone = phones.phone),
     update_trust
-        AS (UPDATE public.user SET suspect = false WHERE suspect = true AND address NOT IN (SELECT * FROM suspect))
-    UPDATE public.user SET suspect = true WHERE address IN (SELECT * FROM suspect)`;
+        AS (UPDATE "user" SET suspect = false WHERE suspect = true AND address NOT IN (SELECT * FROM suspect))
+    UPDATE "user" SET suspect = true WHERE address IN (SELECT * FROM suspect)`;
 
     await sequelize.query(query, {
         type: QueryTypes.UPDATE,
