5dc9abb526d4bb923aff4e2ad4922214caf5b593
João Pedro <joao@impactmarket.com>
2021-08-27 10:35:18 -0300

    update WITH query on verifyUserSuspectActivity

--- src/worker/jobs/cron/user.ts
+++ src/worker/jobs/cron/user.ts
@@ -3,21 +3,38 @@ import { QueryTypes } from 'sequelize';
 import { sequelize } from '../../../database';
 
 export async function verifyUserSuspectActivity(): Promise<void> {
-    const query = `WITH 
-    active_users 
-        AS (SELECT "user".address, "trust".phone
-                FROM "user" 
-                LEFT OUTER JOIN app_user_through_trust AS "through_trust"
-                    ON "user".address = "through_trust"."userAddress"
-                LEFT OUTER JOIN app_user_trust AS "trust"
-                    ON "through_trust"."appUserTrustId" = "trust".id
-                WHERE "user".active = true),
-    suspect
-        AS (SELECT address FROM (SELECT phone FROM active_users GROUP BY phone HAVING count(phone) > 1) as phones 
-                LEFT OUTER JOIN active_users ON active_users.phone = phones.phone),
-    update_trust
-        AS (UPDATE "user" SET suspect = false WHERE suspect = true AND address NOT IN (SELECT * FROM suspect))
-    UPDATE "user" SET suspect = true WHERE address IN (SELECT * FROM suspect)`;
+    const query = `
+        WITH active_users AS (
+            SELECT 
+            "user".address, 
+            "trust".phone 
+            FROM 
+            "user" 
+            LEFT OUTER JOIN app_user_through_trust AS "through_trust" ON "user".address = "through_trust"."userAddress" 
+            LEFT OUTER JOIN app_user_trust AS "trust" ON "through_trust"."appUserTrustId" = "trust".id 
+            WHERE 
+            "user".active = true
+        ), 
+        suspect_phone AS (
+            SELECT 
+            phone, 
+            count(phone) n 
+            FROM 
+            active_users 
+            GROUP BY
+            phone
+        ) 
+        UPDATE 
+            "user" 
+        SET 
+            suspect = CASE WHEN suspect_phone.n > 1 THEN true ELSE false END 
+        FROM 
+            active_users, 
+            suspect_phone 
+        WHERE 
+            "user".address = active_users.address 
+            AND suspect_phone.phone = active_users.phone 
+            AND suspect = CASE WHEN suspect_phone.n > 1 THEN false ELSE true END;`;
 
     await sequelize.query(query, {
         type: QueryTypes.UPDATE,
