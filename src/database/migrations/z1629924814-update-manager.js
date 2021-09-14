6405a14014eb161fe4149bd73fd4b12349d33a6a
Bernardo Vieira <bernardo@impactmarket.com>
2021-09-14 15:12:16 +0100

    rename migration constraint

--- src/database/migrations/z1629924814-update-manager.js
+++ src/database/migrations/z1629924814-update-manager.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    if (process.env.NODE_ENV === 'test') {
-      return;
-    }
-    await queryInterface.removeConstraint('manager', 'manager_address_fkey')
-  },
+    up: async (queryInterface, Sequelize) => {
+        if (process.env.NODE_ENV === 'test') {
+            return;
+        }
+        await queryInterface.removeConstraint('manager', 'manager_user_fkey');
+    },
 
-  down: async (queryInterface, Sequelize) => {}
+    down: async (queryInterface, Sequelize) => {},
 };
