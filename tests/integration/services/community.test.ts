d8373ffce2ba8e2c7de201cd2dc52075e9c12038
João Pedro <joao@impactmarket.com>
2021-10-11 19:13:45 -0300

    fix community test

--- tests/integration/services/community.test.ts
+++ tests/integration/services/community.test.ts
@@ -1667,15 +1667,14 @@ describe('community service', () => {
 
             const tx = randomTx();
 
-            await ManagerFactory(users.slice(0, 2), community[0].publicId),
-                await BeneficiaryService.add(
-                    users[2].address,
-                    users[0].address,
-                    community[0].publicId,
-                    tx,
-                    new Date()
-                );
-
+            await ManagerFactory(users.slice(0, 2), community[0].publicId);
+            await BeneficiaryService.add(
+                users[2].address,
+                users[0].address,
+                community[0].publicId,
+                tx,
+                new Date()
+            );
             await ManagerService.remove(
                 users[0].address,
                 community[0].publicId
