a77548578ff826326bd00da700652257deb70479
João Pedro <joao@impactmarket.com>
2021-10-19 09:32:15 -0300

    create ubiBeneficiaryTransaction migration

--- src/database/migrations/z1605204805-create-ubiBeneficiaryTransaction.js
+++ src/database/migrations/z1605204805-create-ubiBeneficiaryTransaction.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
     up(queryInterface, Sequelize) {
-        return queryInterface.createTable('beneficiarytransaction', {
+        return queryInterface.createTable('ubi_beneficiary_transaction', {
             id: {
                 type: Sequelize.INTEGER,
                 autoIncrement: true,
@@ -35,17 +35,13 @@ module.exports = {
                 type: Sequelize.DATEONLY,
                 allowNull: false,
             },
-            createdAt: {
+            txAt: {
                 allowNull: false,
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
             },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
         });
     },
     down: (queryInterface) => {
-        return queryInterface.dropTable('beneficiarytransaction');
+        return queryInterface.dropTable('ubi_beneficiary_transaction');
     }
 };
---
+++ src/database/migrations/z1634592894-populate-ubiBeneficiaryTransaction.js
@@ -0,0 +1,92 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        if (process.env.NODE_ENV === 'test') {
+            return;
+        }
+
+        const commonColumns = {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            beneficiary: {
+                type: Sequelize.STRING(44),
+                allowNull: false,
+            },
+            withAddress: {
+                type: Sequelize.STRING(44),
+                allowNull: false,
+            },
+            amount: {
+                // https://github.com/sequelize/sequelize/blob/2874c54915b2594225e939809ca9f8200b94f454/lib/dialects/postgres/data-types.js#L102
+                type: Sequelize.DECIMAL(26), // max 99,999,999 - plus 18 decimals
+                allowNull: false,
+            },
+            isFromBeneficiary: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+            tx: {
+                type: Sequelize.STRING(68),
+                unique: true,
+                allowNull: false,
+            },
+            date: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+            },
+        }
+
+        const UbiBeneficiaryTransaction = await queryInterface.sequelize.define(
+            'ubi_beneficiary_transaction',
+            {
+                ...commonColumns,
+                txAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE,
+                },
+            },
+            {
+                tableName: 'ubi_beneficiary_transaction',
+                sequelize: queryInterface.sequelize, 
+            }
+        );
+
+        const BeneficiaryTransaction = await queryInterface.sequelize.define(
+            'beneficiarytransaction',
+            {
+                ...commonColumns,
+                createdAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
+                updatedAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                }
+            },
+            {
+                tableName: 'beneficiarytransaction',
+                sequelize: queryInterface.sequelize, 
+            }
+        );
+
+        const beneficiaryTransaction = await BeneficiaryTransaction.findAll();
+
+        for await(let registry of beneficiaryTransaction) {
+            await UbiBeneficiaryTransaction.create({
+                id: registry.id,
+                beneficiary: registry.beneficiary,
+                withAddress: registry.withAddress,
+                amount: registry.amount,
+                isFromBeneficiary: registry.isFromBeneficiary,
+                tx: registry.tx,
+                date: registry.date,
+                txAt: registry.createdAt,
+            });
+        }
+    },
+    down: (queryInterface, Sequelize) => {},
+};
