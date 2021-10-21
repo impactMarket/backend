154fa85fc8aecde44c6c78d2fabb0341c3d2afc3
João Pedro <joao@impactmarket.com>
2021-10-21 19:44:51 -0300

    add /exchange-rate endpoint

--- src/api/routes/exchangeRates.ts
+++ src/api/routes/exchangeRates.ts
@@ -1,13 +1,52 @@
 import ExchangeRatesService from '@services/app/exchangeRates';
+import { standardResponse } from '@utils/api';
 import { Router, Request, Response } from 'express';
 
 export default (app: Router): void => {
-    const route = Router();
-    app.use('/exchange-rates', route);
 
-    route.get('/', (req: Request, res: Response) => {
+    /**
+     * @deprecated use /exchange-rate
+     */
+    app.get('/exchange-rates', (req: Request, res: Response) => {
         ExchangeRatesService.get()
             .then((rates) => res.send(rates))
             .catch((r) => res.send(r).status(403));
+    });
+
+    /**
+     * @swagger
+     *
+     * /exchange-rate:
+     *   get:
+     *     tags:
+     *       - "rates"
+     *     summary: Get exchange rates.
+     *     responses:
+     *       "200":
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 success:
+     *                   type: boolean
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: object
+     *                     properties:
+     *                       currency:   
+     *                         type: string
+     *                       rate:
+     *                         type: number
+     *     security:
+     *     - api_auth:
+     *       - "write:modify":
+     */
+    app.get('/exchange-rate', (req: Request, res: Response) => {
+        ExchangeRatesService.get()
+            .then((r) => standardResponse(res, 200, true, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
     });
 };
