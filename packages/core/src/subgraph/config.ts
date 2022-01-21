71071ae08c243e8c6a1cc415d75e0ef3de8bc62e
João Pedro <joao@impactmarket.com>
2022-01-21 16:25:26 -0300

    add cross-fetch comment

--- packages/core/src/subgraph/config.ts
+++ packages/core/src/subgraph/config.ts
@@ -1,4 +1,6 @@
+// To use Apollo Client without a browser, is necessary to polyfill or provide a fetch method.
 import 'cross-fetch/polyfill';
+
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 import config from '../config';
