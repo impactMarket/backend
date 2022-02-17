98b3ab47cbb2141bccdf65f1272ccdc291cdf1c5
João Pedro <joao@impactmarket.com>
2022-02-17 10:46:59 -0300

    add PACTDelegateStorageV1 contract

--- packages/core/src/contracts/index.ts
+++ packages/core/src/contracts/index.ts
@@ -4,6 +4,7 @@ import ERC20ABI from './ERC20ABI.json';
 import IPCTDelegate from './IPCTDelegate.json';
 import ImpactMarketABI from './ImpactMarketABI.json';
 import OldCommunityABI from './OldCommunityABI.json';
+import PACTDelegateStorageV1 from './PACTDelegateStorageV1.json'
 
 export {
     CommunityABI,
@@ -12,4 +13,5 @@ export {
     ImpactMarketABI,
     IPCTDelegate,
     OldCommunityABI,
+    PACTDelegateStorageV1,
 };
--- services/proposals/handler.ts
+++ services/proposals/handler.ts
@@ -1,24 +1,46 @@
 import axios from 'axios';
+import { ethers } from 'ethers';
+import { contracts } from '@impactmarket/core';
 
 export const verifyProposals = async (event: any, context: any) => {
-  const resp = await axios.post(process.env.WEBHOOK_URL!, {
-    embeds: [{
-      type: 'rich',
-      title: 'New Proposal',
-      description: 'New proposal for the community: TESTE',
-      url: 'https://www.impactmarket.com/',
-      color: 39423,
-      timestamp: null,
-      fields: [],
-      thumbnail: null,
-      image: null,
-      video: null,
-      author: null,
-      provider: null,
-      footer: null,
-      files: []
-    }]
-  });
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.CHAIN_JSON_RPC_URL
+    );
+  
+    const DAOContract = new ethers.Contract(
+      process.env.PACT_DELEGATOR!,
+      contracts.PACTDelegateStorageV1,
+      provider
+    );
 
-  return !!resp
+    const proposalCount = await DAOContract.proposalCount();
+    const proposals = await DAOContract.proposals(process.env.PACT_DELEGATOR!);
+
+    console.log(parseInt(proposalCount))
+    console.log(proposals)
+
+    const resp = await axios.post(process.env.WEBHOOK_URL!, {
+      embeds: [{
+        type: 'rich',
+        title: 'New Proposal',
+        description: 'New proposal for the community: TESTE',
+        url: 'https://www.impactmarket.com/',
+        color: 39423,
+        timestamp: null,
+        fields: [],
+        thumbnail: null,
+        image: null,
+        video: null,
+        author: null,
+        provider: null,
+        footer: null,
+        files: []
+      }]
+    });
+    return !!resp
+  } catch (error) {
+    console.log('erro ==> ', error);
+    return false;
+  }
 }
