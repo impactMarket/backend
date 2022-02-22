c0782c00443828e44b0912b531ca8bad1e0ab669
João Pedro <joao@impactmarket.com>
2022-02-22 15:21:09 -0300

    add prismic

--- services/proposals/handler.ts
+++ services/proposals/handler.ts
@@ -1,58 +1,73 @@
 import axios from 'axios';
 import { ethers } from 'ethers';
 import { contracts } from '@impactmarket/core';
+import * as prismic from '@prismicio/client'
+
+const endpoint = prismic.getEndpoint(process.env.PRISMIC_REPO!);
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+const client = prismic.createClient(endpoint, { accessToken });
 
 export const verifyProposals = async (event: any, context: any) => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.CHAIN_JSON_RPC_URL
-  );
-
-  const DAOContract = new ethers.Contract(
-    process.env.PACT_DELEGATOR!,
-    contracts.PACTDelegateABI,
-    provider
-  );
-
-  const proposalCount = parseInt(await DAOContract.proposalCount());
-
-  const lastHour = new Date(new Date().getTime() - 60*60*1000).getTime();
-  const newProposals: any = [];
-  for(let i = proposalCount-1; ; i--) {
-    const proposal = await DAOContract.proposals(i);
-    const block = await getBlockByNumber(proposal.startBlock._hex);
-    // const block = await provider.getBlock(parseInt(proposal.startBlock));
-    const blockTimestamp = parseInt(block.timestamp) * 1000;
-
-    if(blockTimestamp >= lastHour) {
-      if(!proposal.canceled && !proposal.executed) {
-        newProposals.push(proposal);
-      }
-    } else {
-      break
-    }
-  }
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.CHAIN_JSON_RPC_URL
+    );
   
-  const promises = newProposals.map((proposal) =>
-    axios.post(process.env.DISCORD_WEBHOOK!, {
-      embeds: [{
-        type: 'rich',
-        title: 'New Proposal',
-        description: 'New proposal opened: ' + parseInt(proposal.id),
-        url: 'https://www.impactmarket.com/',
-        color: 39423,
-        timestamp: null,
-        fields: [],
-        thumbnail: null,
-        image: null,
-        video: null,
-        author: null,
-        provider: null,
-        footer: null,
-        files: []
-      }]
-    })
-  );
-  await Promise.all(promises);
+    const DAOContract = new ethers.Contract(
+      process.env.PACT_DELEGATOR!,
+      contracts.PACTDelegateABI,
+      provider
+    );
+  
+    const proposalCount = parseInt(await DAOContract.proposalCount());
+  
+    const lastHour = new Date(new Date().getTime() - 60*60*1000).getTime();
+    const newProposals: any = [];
+    for(let i = proposalCount-1; ; i--) {
+      const proposal = await DAOContract.proposals(i);
+      const block = await getBlockByNumber(proposal.startBlock._hex);
+      const blockTimestamp = parseInt(block.timestamp) * 1000;
+  
+      if(blockTimestamp >= lastHour) {
+        if(!proposal.canceled && !proposal.executed) {
+          newProposals.push(proposal);
+        }
+      } else {
+        break
+      }
+    }
+  
+    if(newProposals.length > 0) {
+      const data = await client.getAllByType(process.env.PRISMIC_CUSTOM_TYPE!, { lang: process.env.PRISMIC_LANGUAGE });  
+      const title = data[0].data.opened_proposal_title
+      const description = data[0].data.opened_proposal_description
+      const promises = newProposals.map((proposal) =>
+        axios.post(process.env.DISCORD_WEBHOOK!, {
+          embeds: [{
+            type: 'rich',
+            title: title,
+            description: description.replace('{id}', parseInt(proposal.id)),
+            url: process.env.GOVERNACE_SYSTEM_URL,
+            color: 39423,
+            timestamp: null,
+            fields: [],
+            thumbnail: null,
+            image: null,
+            video: null,
+            author: null,
+            provider: null,
+            footer: null,
+            files: []
+          }]
+        })
+      );
+      await Promise.all(promises);
+    } else {
+      console.info('No new proposals');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const getBlockByNumber = async (blockNumber: string) => {
