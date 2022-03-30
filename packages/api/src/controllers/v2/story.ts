2d5ebf648afbbd1887fdcee4546b5d3a24e81fc9
Bernardo Vieira <bernardo@impactmarket.com>
2022-03-30 23:15:19 +0100

    stories v2 draft

---
+++ packages/api/src/controllers/v2/story.ts
@@ -0,0 +1,133 @@
+import { config, database, services } from '@impactmarket/core';
+import { Request, Response } from 'express';
+
+import { RequestWithUser } from '../../middlewares/core';
+import { standardResponse } from '../../utils/api';
+
+class StoryController {
+    storyService = new services.StoryServiceV2();
+
+    getPresignedUrlMedia = (req: RequestWithUser, res: Response) => {
+        if (req.user === undefined) {
+            standardResponse(res, 401, false, '', {
+                error: {
+                    name: 'USER_NOT_FOUND',
+                    message: 'User not identified!',
+                },
+            });
+            return;
+        }
+        this.storyService
+            .getPresignedUrlMedia(req.params.mime)
+            .then((r) => standardResponse(res, 200, true, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
+    };
+
+    add = async (req: RequestWithUser, res: Response) => {
+        if (req.user === undefined) {
+            standardResponse(res, 401, false, '', {
+                error: {
+                    name: 'USER_NOT_FOUND',
+                    message: 'User not identified!',
+                },
+            });
+            return;
+        }
+        const { communityId, message, storyMediaPath } = req.body;
+
+        this.storyService
+            .add(req.user.address, {
+                communityId,
+                message,
+                storyMediaPath,
+            })
+            .then((r) => standardResponse(res, 200, true, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
+    };
+
+    love = (req: RequestWithUser, res: Response) => {
+        if (req.user === undefined) {
+            standardResponse(res, 401, false, '', {
+                error: {
+                    name: 'USER_NOT_FOUND',
+                    message: 'User not identified!',
+                },
+            });
+            return;
+        }
+        this.storyService
+            .love(req.user.address, parseInt(req.params.id, 10))
+            .then((r) => standardResponse(res, 200, true, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
+    };
+
+    remove = (req: RequestWithUser, res: Response) => {
+        if (req.user === undefined) {
+            standardResponse(res, 401, false, '', {
+                error: {
+                    name: 'USER_NOT_FOUND',
+                    message: 'User not identified!',
+                },
+            });
+            return;
+        }
+        this.storyService
+            .remove(parseInt(req.params.id, 10), req.user.address)
+            .then((r) => standardResponse(res, 200, r !== 0, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
+    };
+
+    list = (req: RequestWithUser, res: Response) => {
+        const { user } = req.query;
+        if (user) {
+            if (req.user === undefined || user !== req.user.address) {
+                standardResponse(res, 401, false, '', {
+                    error: {
+                        name: 'NOT_ALLOWED',
+                        message: 'Not allowed!',
+                    },
+                });
+                return;
+            }
+            this.storyService
+                .listByUser(req.user.address, req.query)
+                .then((r) =>
+                    standardResponse(res, 200, true, r.content, {
+                        count: r.count,
+                    })
+                )
+                .catch((e) =>
+                    standardResponse(res, 400, false, '', { error: e })
+                );
+        } else {
+            this.storyService
+                .list(req.query)
+                .then((r) =>
+                    standardResponse(res, 200, true, r.content, {
+                        count: r.count,
+                    })
+                )
+                .catch((e) =>
+                    standardResponse(res, 400, false, '', { error: e })
+                );
+        }
+    };
+
+    inapropriate = (req: RequestWithUser, res: Response) => {
+        if (req.user === undefined) {
+            standardResponse(res, 401, false, '', {
+                error: {
+                    name: 'USER_NOT_FOUND',
+                    message: 'User not identified!',
+                },
+            });
+            return;
+        }
+        this.storyService
+            .inapropriate(req.user.address, parseInt(req.params.id, 10))
+            .then((r) => standardResponse(res, 200, true, r))
+            .catch((e) => standardResponse(res, 400, false, '', { error: e }));
+    };
+}
+
+export default StoryController;
