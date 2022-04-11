a224dfb4e27162c35d48bddeb5ec71760de1904c
João Pedro <joao@impactmarket.com>
2022-04-11 12:06:53 -0300

    add love and inapropriate endpoints

--- packages/core/src/interfaces/story/storyUserReport.ts
+++ packages/core/src/interfaces/story/storyUserReport.ts
@@ -2,9 +2,11 @@ export interface StoryUserReport {
     id: number;
     contentId: number;
     address: string;
+    typeId?: number;
 }
 
 export interface StoryUserReportCreation {
     contentId: number;
     address: string;
+    typeId?: number;
 }
