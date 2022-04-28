f78b6a221673d5dd1ce7b0610ae8cae6c0566420
João Pedro <joao@impactmarket.com>
2022-04-28 14:37:42 -0300

    count communities by country

--- packages/api/src/routes/v2/community/details.ts
+++ packages/api/src/routes/v2/community/details.ts
@@ -2,9 +2,48 @@ import { Router } from 'express';
 
 import { CommunityController } from '../../../controllers/v2/community/details';
 import { optionalAuthentication } from '../../../middlewares';
+import { database } from '@impactmarket/core';
 
 export default (route: Router): void => {
     const controller = new CommunityController();
+
+    /**
+     * @swagger
+     *
+     * /community/count:
+     *   get:
+     *     tags:
+     *       - "community"
+     *     summary: Count grouped communities communities
+     *     parameters:
+     *       - in: query
+     *         name: groupBy
+     *         schema:
+     *           type: string
+     *           enum: [country]
+     *         required: true
+     *         description: count communities by a grouped value
+     *     responses:
+     *       "200":
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   unknown:
+     *                     type: string
+     *                     description: this variable name changes depending on the request. If the groupBy is "country" then this variable is named "country" and the value is the country.
+     *                   count:
+     *                     type: string
+     */
+     route.get(
+        '/count/:query?',
+        database.cacheWithRedis('5 minutes', database.cacheOnlySuccess),
+        controller.count
+    );
 
     /**
      * @swagger
