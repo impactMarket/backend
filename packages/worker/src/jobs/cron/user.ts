3e1dc0ec5ac42f2f1d1e6f63d3309c05742c6bb6
Joao Pedro da Silva <joao@impactmarket.com>
2022-05-10 20:39:52 -0300

    remove unused cron job

--- packages/worker/src/jobs/cron/user.ts
+++ packages/worker/src/jobs/cron/user.ts
@@ -1,43 +1,6 @@
 import { interfaces, database } from '@impactmarket/core';
 import { QueryTypes, Op } from 'sequelize';
 
-export async function verifyUserSuspectActivity(): Promise<void> {
-    const query = `
-        WITH active_users AS (
-            SELECT 
-            "user".address, 
-            "user".phone 
-            FROM 
-            "app_user" as "user"
-            WHERE 
-            "user".active = true
-        ), 
-        suspect_phone AS (
-            SELECT 
-            phone, 
-            count(phone) n 
-            FROM 
-            active_users 
-            GROUP BY
-            phone
-        ) 
-        UPDATE 
-            "app_user" as "user"
-        SET 
-            suspect = CASE WHEN suspect_phone.n > 1 THEN true ELSE false END 
-        FROM 
-            active_users, 
-            suspect_phone 
-        WHERE 
-            "user".address = active_users.address 
-            AND suspect_phone.phone = active_users.phone 
-            AND suspect = CASE WHEN suspect_phone.n > 1 THEN false ELSE true END;`;
-
-    await database.sequelize.query(query, {
-        type: QueryTypes.UPDATE,
-    });
-}
-
 export async function verifyDeletedAccounts(): Promise<void> {
     const t = await database.sequelize.transaction();
     try {
