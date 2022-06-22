8807ba821f72f402575c04b1944c6d7e05f2f17c
Joao Pedro da Silva <joao@impactmarket.com>
2022-06-22 18:20:15 -0300

    fix list communities

--- packages/core/src/services/ubi/community/list.ts
+++ packages/core/src/services/ubi/community/list.ts
@@ -526,7 +526,10 @@ export class CommunityListService {
                         query.offset
                             ? parseInt(query.offset, 10)
                             : config.defaultOffset
-                    },`,
+                    },
+                    where: {
+                        state: 0
+                    }`,
                 `id, ${orderBy}`
             );
         }
