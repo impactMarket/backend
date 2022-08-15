30d177902ef132550268f43e199415256e06264a
Joao Pedro da Silva <joao@impactmarket.com>
2022-08-15 14:16:15 -0300

    fix eslint and sonar

--- services/orchestrator/handler.ts
+++ services/orchestrator/handler.ts
@@ -3,44 +3,54 @@ import axios from 'axios';
 
 const lambda = new AWS.Lambda();
 const API_KEY = process.env.API_KEY;
+const HEROKU_URL = process.env.HEROKU_URL;
 
 const getHerokuConfigVars = async (appId: string) => {
-    const response = await axios.get(`https://api.heroku.com/apps/${appId}/config-vars`, {
-        headers: {
-          'Authorization': `Bearer ${API_KEY}`,
-          'Accept': 'application/vnd.heroku+json; version=3'
+    const response = await axios.get(
+        `${HEROKU_URL}/apps/${appId}/config-vars`,
+        {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                Accept: 'application/vnd.heroku+json; version=3',
+            },
         }
-    });
+    );
     return response.data;
-}
+};
 
 export const herokuTrigger = async (event: any, context: any) => {
     try {
         const requestBody = JSON.parse(event.body);
         console.log(requestBody);
 
-        const herokuConfigVars = await getHerokuConfigVars(requestBody.data.app.id);
+        const herokuConfigVars = await getHerokuConfigVars(
+            requestBody.data.app.id
+        );
         const { REDIS_URL, DATABASE_URL } = herokuConfigVars;
 
         const listFunctions = await lambda.listFunctions().promise();
         listFunctions.Functions?.forEach(async (res) => {
-            const config = await lambda.getFunctionConfiguration({ FunctionName: res.FunctionName! }).promise();
-            await lambda.updateFunctionConfiguration({
-                FunctionName: res.FunctionName!,
-                Environment: {
-                    Variables: {
-                        ...config.Environment?.Variables,
-                        REDIS_URL,
-                        DATABASE_URL,
-                    }
-                }
-            }).promise();
+            const config = await lambda
+                .getFunctionConfiguration({ FunctionName: res.FunctionName! })
+                .promise();
+            await lambda
+                .updateFunctionConfiguration({
+                    FunctionName: res.FunctionName!,
+                    Environment: {
+                        Variables: {
+                            ...config.Environment?.Variables,
+                            REDIS_URL,
+                            DATABASE_URL,
+                        },
+                    },
+                })
+                .promise();
         });
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-              message: "Env vars updated",
+                message: 'Env vars updated',
             }),
         };
     } catch (error) {
