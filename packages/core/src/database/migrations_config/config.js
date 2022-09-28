6d827263362e91c13684cc74281262bcc5cafbb9
Joao Pedro da Silva <joao@impactmarket.com>
2022-09-28 11:56:20 -0300

    fix config sequelize

--- packages/core/src/database/migrations_config/config.js
+++ packages/core/src/database/migrations_config/config.js
@@ -35,7 +35,7 @@ module.exports = {
     development: {
         username: 'postgres',
         password: 'mysecretpassword',
-        database: 'databasedump',
+        database: 'impactmarket',
         host: 'localhost',
         port: 5432,
         dialect: 'postgres',
