e20f89e5a358fbfb0fcc5081af83823558b29847
Joao Pedro da Silva <joao@impactmarket.com>
2022-10-11 18:31:48 -0300

    list reports by community

--- packages/core/src/services/app/user/index.ts
+++ packages/core/src/services/app/user/index.ts
@@ -294,7 +294,11 @@ export default class UserService {
 
     public async getReport(
         user: string,
-        query: { offset?: string; limit?: string }
+        query: {
+            offset?: string;
+            limit?: string;
+            community?: number;
+        }
     ) {
         const userRoles = await getUserRoles(user);
 
@@ -309,6 +313,32 @@ export default class UserService {
         }
 
         const communities = userRoles.ambassador.communities;
+        const communityId = query.community;
+        let addresses: string[] = [];
+
+        if (communityId) {
+            const community = await models.community.findOne({
+                attributes: ['contractAddress'],
+                where: {
+                    id: communityId,
+                },
+            });
+
+            if (
+                !community?.contractAddress ||
+                communities.indexOf(
+                    community?.contractAddress?.toLocaleLowerCase()
+                ) === -1
+            ) {
+                throw new BaseError(
+                    'NOT_AMBASSADOR',
+                    'user is not an ambassador of this community'
+                );
+            }
+            addresses.push(community.contractAddress);
+        } else {
+            addresses = communities;
+        }
 
         return models.anonymousReport.findAndCountAll({
             include: [
@@ -323,7 +353,7 @@ export default class UserService {
                     as: 'community',
                     where: {
                         contractAddress: {
-                            [Op.in]: communities.map((c) => getAddress(c)),
+                            [Op.in]: addresses.map((c) => getAddress(c)),
                         },
                     },
                 },
