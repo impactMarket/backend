66231e7e075de458df2d5543eee5cd03962e173f
Joao Pedro da Silva <joao@impactmarket.com>
2023-01-24 10:31:25 -0300

    fix eslint

--- packages/core/src/subgraph/config.ts
+++ packages/core/src/subgraph/config.ts
@@ -1,16 +1,15 @@
-import { fetch } from 'cross-fetch';
 import { ApolloClient, InMemoryCache } from 'apollo-boost';
 import { HttpLink } from 'apollo-link-http';
+import { fetch } from 'cross-fetch';
 
 import config from '../config';
-
 
 export const clientDAO = new ApolloClient({
     link: new HttpLink({
         uri: config.subgraphUrl,
         fetch,
     }),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
 });
 
 export const clientCouncil = new ApolloClient({
@@ -18,5 +17,5 @@ export const clientCouncil = new ApolloClient({
         uri: config.councilSubgraphUrl,
         fetch,
     }),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
 });
