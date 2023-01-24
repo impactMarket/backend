3df9006d32c6babe7709b1e73500b1ae09416a20
Joao Pedro da Silva <joao@impactmarket.com>
2023-01-24 10:20:47 -0300

    add fetch on apollo-boost

--- packages/core/src/subgraph/config.ts
+++ packages/core/src/subgraph/config.ts
@@ -1,16 +1,22 @@
-// To use Apollo Client without a browser, is necessary to polyfill or provide a fetch method.
-import 'cross-fetch/polyfill';
-
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { fetch } from 'cross-fetch';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { HttpLink } from 'apollo-link-http';
 
 import config from '../config';
 
+
 export const clientDAO = new ApolloClient({
-    uri: config.subgraphUrl,
-    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: config.subgraphUrl,
+        fetch,
+    }),
+    cache: new InMemoryCache()
 });
 
 export const clientCouncil = new ApolloClient({
-    uri: config.councilSubgraphUrl,
-    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: config.councilSubgraphUrl,
+        fetch,
+    }),
+    cache: new InMemoryCache()
 });
