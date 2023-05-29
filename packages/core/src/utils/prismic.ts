091f9d1e11491fb1858aa7c996be02a99e3fc51f
Bernardo Vieira <bernardo@impactmarket.com>
2023-05-29 16:52:27 +0100

    add support for translated validation messages (#641)

--- packages/core/src/utils/prismic.ts
+++ packages/core/src/utils/prismic.ts
@@ -1,6 +1,6 @@
 import * as prismic from '@prismicio/client';
 
-const endpoint = prismic.getEndpoint(process.env.PRISMIC_REPO!);
+const endpoint = prismic.getRepositoryEndpoint(process.env.PRISMIC_REPO!);
 const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
 
 export const client = prismic.createClient(endpoint, { accessToken });
