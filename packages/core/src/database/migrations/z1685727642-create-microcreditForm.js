5484f986febf0a606e33400cc98c83dff695f8d9
Bernardo Vieira <bernardo@impactmarket.com>
2023-07-04 14:08:55 +0100

    fix migration

--- packages/core/src/database/migrations/z1685727642-create-microcreditForm.js
+++ packages/core/src/database/migrations/z1685727642-create-microcreditForm.js
@@ -29,7 +29,7 @@ module.exports = {
             status: {
                 type: Sequelize.ENUM('pending', 'submitted', 'in-review', 'approved', 'rejected'),
                 allowNull: false,
-                defaultValue: false
+                defaultValue: 'pending'
             },
             createdAt: {
                 type: Sequelize.DATE,
