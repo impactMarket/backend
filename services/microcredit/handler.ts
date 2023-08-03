836ea6c7b76f01de1f69e4f7c312cae526f604f9
Joao Pedro da Silva <joao@impactmarket.com>
2023-08-03 11:00:40 -0300

    update lambda name

--- services/microcredit/handler.ts
+++ services/microcredit/handler.ts
@@ -3,6 +3,6 @@ import { updateBorrowers } from './src/borrowers';
 global.btoa = (str: string) => Buffer.from(str, 'binary').toString('base64');
 global.atob = (str: string) => Buffer.from(str, 'base64').toString('binary');
 
-export const start = async (event, context) => {
+export const borrowers = async (event, context) => {
     await updateBorrowers();
 };
