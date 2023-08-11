35099d9746cf0e89706ad2eb327917fd25310246
Joao Pedro da Silva <joao@impactmarket.com>
2023-08-11 06:52:02 -0300

    [bugfix] update borrowers lambda (#809)

--- packages/core/src/subgraph/queries/microcredit.ts
+++ packages/core/src/subgraph/queries/microcredit.ts
@@ -389,7 +389,6 @@ export const getUserLastLoanStatusFromSubgraph = async (userAddress: string): Pr
 export const getBorrowers = async (
     query: SubgraphGetBorrowersQuery
 ): Promise<{
-    count: number;
     borrowers: {
         // optional so it can be deleted from object!
         id: string;
@@ -453,30 +452,27 @@ export const getBorrowers = async (
         return JSON.parse(cacheResults);
     }
 
-    const [count, response] = await Promise.all([
-        countGetBorrowers(query),
-        axiosMicrocreditSubgraph.post<
-            any,
-            {
+    const response = await axiosMicrocreditSubgraph.post<
+        any,
+        {
+            data: {
                 data: {
-                    data: {
-                        borrowers: {
-                            id: string;
-                            lastLoanAmount: string;
-                            lastLoanPeriod: number;
-                            lastLoanDailyInterest: number;
-                            lastLoanClaimed: number;
-                            lastLoanRepaid: string;
-                            lastLoanLastRepayment: number;
-                            lastLoanLastRepaymentAmount: string;
-                            lastLoanLastDebt: string;
-                            lastLoanStatus: number;
-                        }[];
-                    };
+                    borrowers: {
+                        id: string;
+                        lastLoanAmount: string;
+                        lastLoanPeriod: number;
+                        lastLoanDailyInterest: number;
+                        lastLoanClaimed: number;
+                        lastLoanRepaid: string;
+                        lastLoanLastRepayment: number;
+                        lastLoanLastRepaymentAmount: string;
+                        lastLoanLastDebt: string;
+                        lastLoanStatus: number;
+                    }[];
                 };
-            }
-        >('', graphqlQuery)
-    ]);
+            };
+        }
+    >('', graphqlQuery);
 
     const borrowers = response.data?.data.borrowers.map(borrower => ({
         loan: {
@@ -492,9 +488,9 @@ export const getBorrowers = async (
         },
         id: borrower.id
     }));
-    redisClient.set(graphqlQuery.query, JSON.stringify({ count, borrowers }), 'EX', intervalsInSeconds.twoMins);
+    redisClient.set(graphqlQuery.query, JSON.stringify({ borrowers }), 'EX', intervalsInSeconds.twoMins);
 
-    return { count, borrowers };
+    return { borrowers };
 };
 
 export const getBorrowerLoansCount = async (borrower: string): Promise<number> => {
