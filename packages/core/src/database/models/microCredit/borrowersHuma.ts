132c1d4dffe26aec5b40232cd8cd33f3e986a47e
Bernardo Vieira <bernardo@impactmarket.com>
2023-08-22 15:55:13 +0100

    add borrowers huma model

---
+++ packages/core/src/database/models/microCredit/borrowersHuma.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+
+import { DbModels } from '../../../database/db';
+import {
+    MicroCreditBorrowersHuma,
+    MicroCreditBorrowersHumaCreation
+} from '../../../interfaces/microCredit/borrowersHuma';
+
+export class MicroCreditBorrowersHumaModel extends Model<MicroCreditBorrowersHuma, MicroCreditBorrowersHumaCreation> {
+    public id!: number;
+    public userId!: number;
+    public humaRWRReferenceId!: string;
+}
+
+export function initializeMicroCreditBorrowersHuma(sequelize: Sequelize): typeof MicroCreditBorrowersHumaModel {
+    const { appUser } = sequelize.models as DbModels;
+    MicroCreditBorrowersHumaModel.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: appUser,
+                    key: 'id'
+                },
+                onDelete: 'CASCADE',
+                allowNull: false
+            },
+            humaRWRReferenceId: {
+                allowNull: false,
+                type: DataTypes.STRING(64)
+            }
+        },
+        {
+            tableName: 'microcredit_borrowers_huma',
+            modelName: 'microCreditBorrowersHuma',
+            sequelize,
+            timestamps: false
+        }
+    );
+    return MicroCreditBorrowersHumaModel;
+}
---
+++ packages/core/src/interfaces/microCredit/borrowersHuma.ts
@@ -0,0 +1,10 @@
+export interface MicroCreditBorrowersHuma {
+    id: number;
+    userId: number;
+    humaRWRReferenceId: string;
+}
+
+export interface MicroCreditBorrowersHumaCreation {
+    userId: number;
+    humaRWRReferenceId: string;
+}
