779d27396fb71c298ef86c9c0d01f49a20bb516f
Bernardo Vieira <bernardo@impactmarket.com>
2023-09-08 18:30:24 +0100

    [chore] partially improve subgraph query (#857)

---
+++ packages/core/src/subgraph/queries/utils.ts
@@ -0,0 +1,38 @@
+import { axiosSubgraph } from '../config';
+import { intervalsInSeconds } from '../../types';
+import { redisClient } from '../../database';
+
+export type SubgraphResponse<T> = { data: T };
+
+export function queryAndTransformResponse<T>(
+    graphqlQuery: { operationName: string; query: string },
+    transform?: any,
+    options?: { cacheTimeout?: number; cacheKey?: string }
+): Promise<T>;
+
+export function queryAndTransformResponse<T, Y>(
+    graphqlQuery: { operationName: string; query: string },
+    transform: (value: T) => Y,
+    options?: { cacheTimeout?: number; cacheKey?: string }
+): Promise<Y>;
+
+export async function queryAndTransformResponse<T, Y>(
+    graphqlQuery: { operationName: string; query: string },
+    transform?: (value: T) => Y,
+    options?: { cacheTimeout?: number; cacheKey?: string }
+): Promise<T | Y> {
+    const cacheResults = await redisClient.get(graphqlQuery.query);
+    if (cacheResults) {
+        return JSON.parse(cacheResults);
+    }
+    const response = await axiosSubgraph.post<SubgraphResponse<T>>('', graphqlQuery);
+    const transformedResponse = transform ? transform(response.data.data) : response.data.data;
+    redisClient.set(
+        graphqlQuery.query,
+        JSON.stringify(transformedResponse),
+        'EX',
+        options?.cacheTimeout || intervalsInSeconds.oneHour
+    );
+
+    return transformedResponse;
+}
