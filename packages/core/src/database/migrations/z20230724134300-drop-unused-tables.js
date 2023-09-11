902628d00fc38c5c1e2e30e19a63bf1061d046a0
Bernardo Vieira <bernardo@impactmarket.com>
2023-09-11 21:14:00 +0100

    [bugfix] fix drop tables migration (#874)

--- packages/core/src/database/migrations/z20230724134300-drop-unused-tables.js
+++ packages/core/src/database/migrations/z20230724134300-drop-unused-tables.js
@@ -7,19 +7,47 @@ module.exports = {
             return;
         }
 
+        // some of this are very old tables without any usage
+        // some might not exist anymore in some environments
+
         try {
             await queryInterface.removeConstraint('story_content', 'story_content_mediaMediaId_fkey');
-        } catch (_) {}
-        await queryInterface.removeColumn('story_content', 'mediaMediaId');
-
-        await queryInterface.sequelize.query(`drop table ubi_claim`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table beneficiarytransaction`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table ubi_beneficiary_transaction`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table app_media_content`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (e) { console.error(e); }
+        try {
+            await queryInterface.removeConstraint('user', 'user_avatarMediaId_fkey');
+        } catch (e) { console.error(e); }
+        try {
+            await queryInterface.removeColumn('story_content', 'mediaMediaId');
+        } catch (e) { console.error(e); }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_claim`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table beneficiarytransaction`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_beneficiary_transaction`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_organization_social_media`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_community_organization`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_organization`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
         await queryInterface.sequelize.query(`drop table app_media_thumbnail`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table beneficiary`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table manager`, { type: Sequelize.QueryTypes.DELETE });
-        await queryInterface.sequelize.query(`drop table ubi_beneficiary_registry`, { type: Sequelize.QueryTypes.DELETE });
+        await queryInterface.sequelize.query(`drop table app_media_content`, { type: Sequelize.QueryTypes.DELETE });
+        try {
+            await queryInterface.sequelize.query(`drop table beneficiary`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table manager`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
+        try {
+            await queryInterface.sequelize.query(`drop table ubi_beneficiary_registry`, { type: Sequelize.QueryTypes.DELETE });
+        } catch (_) { }
     },
     async down(queryInterface, Sequelize) {}
 };
