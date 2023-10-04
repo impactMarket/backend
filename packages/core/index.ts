618c4d5a0f932bfacca2a932763d474b869eb67b
Joao Pedro da Silva <joao@impactmarket.com>
2023-10-04 06:17:48 -0300

    [refactor] move subscriber to worker (#943)

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,10 +3,9 @@ import * as database from './src/database';
 import * as interfaces from './src/interfaces';
 import * as services from './src/services';
 import * as subgraph from './src/subgraph';
-import * as subscriber from './src/subscriber';
 import * as tests from './tests';
 import * as types from './src/types';
 import * as utils from './src/utils';
 import config from './src/config';
 
-export { config, contracts, database, interfaces, services, utils, types, tests, subgraph, subscriber };
+export { config, contracts, database, interfaces, services, utils, types, tests, subgraph };
