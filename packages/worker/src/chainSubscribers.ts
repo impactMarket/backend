fbee1f78866e93664eeee95ec49fc193ca069ce2
Bernardo Vieira <bernardo@impactmarket.com>
2023-10-31 12:48:58 +0000

    clear cache

--- packages/worker/src/chainSubscribers.ts
+++ packages/worker/src/chainSubscribers.ts
@@ -549,8 +549,14 @@ class ChainSubscribers {
             } else if (parsedLog.name === 'UserAddressChanged') {
                 utils.Logger.info('UserAddressChanged event');
                 const newUserAddress = parsedLog.args[1];
-
-                const [oldUser, [newUser]] = await Promise.all([
+                const transactionsReceipt = await this.provider.getTransaction(log.transactionHash);
+                const [loanManagerUser, oldUser, [newUser]] = await Promise.all([
+                    database.models.appUser.findOne({
+                        attributes: ['id'],
+                        where: {
+                            address: getAddress(transactionsReceipt.from)
+                        }
+                    }),
                     database.models.appUser.findOne({
                         attributes: ['id'],
                         where: {
@@ -581,6 +587,13 @@ class ChainSubscribers {
                             transaction
                         }
                     );
+                    this._waitForSubgraphToIndex(log).then(() => {
+                        utils.cache.cleanUserRolesCache(userAddress);
+                        if (loanManagerUser) {
+                            utils.cache.cleanMicroCreditBorrowersCache(loanManagerUser.id);
+                            utils.cache.cleanMicroCreditApplicationsCache(loanManagerUser.id);
+                        }
+                    });
                 }
             }
         } catch (error) {
