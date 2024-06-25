90cdbed2851f55956dfe6b884598ca3c3e83da65
AnotherDev <anotherdev.eth@gmail.com>
2024-06-25 18:50:42 -0500

    feat: add circulatingSupply controler

---
+++ packages/api/src/controllers/v2/circulatingSupply.ts
@@ -0,0 +1,31 @@
+import { BigNumber } from 'bignumber.js';
+import { Contract } from '@ethersproject/contracts';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Request, Response } from 'express';
+import { config, contracts } from '@impactmarket/core';
+
+// this is required for external services to access the circulating supply
+export const circulatingSupply = async (_req: Request, res: Response) => {
+    const decimals = new BigNumber(10).pow(18);
+    const pact = new Contract(
+        config.contractAddresses.pact,
+        contracts.ERC20ABI,
+        new JsonRpcProvider(config.jsonRpcUrl)
+    );
+    const airgrabPACTBalance = await pact.balanceOf(config.contractAddresses.airgrab);
+    const daoPACTBalance = await pact.balanceOf(config.contractAddresses.dao);
+    const donationMinerPACTBalance = await pact.balanceOf(config.contractAddresses.donationMiner);
+    const impactLabsPACTBalance = await pact.balanceOf(config.contractAddresses.impactLabs);
+    const idoPACTBalance = await pact.balanceOf(config.contractAddresses.ido);
+    const totalSupply = new BigNumber(100_000_000_000).multipliedBy(decimals);
+    const circulatingSupply = new BigNumber(totalSupply)
+        .minus(airgrabPACTBalance.toString())
+        .minus(daoPACTBalance.toString())
+        .minus(donationMinerPACTBalance.toString())
+        .minus(impactLabsPACTBalance.toString())
+        .minus(idoPACTBalance.toString());
+    const response = circulatingSupply.dividedBy(decimals).toNumber();
+
+    // answer
+    res.send(response.toString());
+};
