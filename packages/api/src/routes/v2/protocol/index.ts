4ce34e537e7d5be1915e2b74cfbfd6e16d4b7a18
Luis_ <73004377+Another-DevX@users.noreply.github.com>
2024-06-25 20:58:14 -0400

    Implement PactV2 circulating supply (#1126)
    Co-authored-by: Bernardo <19441097+obernardovieira@users.noreply.github.com>
    Co-authored-by: AnotherDev <anotherdev.eth@gmail.com>

--- packages/api/src/routes/v2/protocol/index.ts
+++ packages/api/src/routes/v2/protocol/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import microcredit from './microcredit';
+import circulatingSupply from './circulatingSupply';
 
 export default (app: Router): void => {
     const route = Router();
     app.use('/protocol', route);
 
     microcredit(route);
+    circulatingSupply(route);
+
 };
