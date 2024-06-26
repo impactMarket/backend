fc2f76fa8f56bc9f0898e9dd2bef65ef70bc13fb
AnotherDev <anotherdev.eth@gmail.com>
2024-06-25 19:33:11 -0500

    feat: add top holders modification

--- packages/api/src/controllers/v2/circulatingSupply.ts
+++ packages/api/src/controllers/v2/circulatingSupply.ts
@@ -12,18 +12,24 @@ export const circulatingSupply = async (_req: Request, res: Response) => {
         contracts.ERC20ABI,
         new JsonRpcProvider(config.jsonRpcUrl)
     );
-    const airgrabPACTBalance = await pact.balanceOf(config.contractAddresses.airgrab);
-    const daoPACTBalance = await pact.balanceOf(config.contractAddresses.dao);
-    const donationMinerPACTBalance = await pact.balanceOf(config.contractAddresses.donationMiner);
-    const impactLabsPACTBalance = await pact.balanceOf(config.contractAddresses.impactLabs);
-    const idoPACTBalance = await pact.balanceOf(config.contractAddresses.ido);
+    const topHolders = [
+        '0xBD11CaeA0a854ba328e202ceD2F0269fD8027c6e',
+        '0x59aAc0b8bd03b7Ba9D391Eb989c3Ea8CdE638144',
+        '0x73cD8626b3cD47B009E68380720CFE6679A3Ec3D',
+        '0x3844cb665cf676B1Eb7C896E04D3E9eC3BAB5a75',
+        '0x213962Ba8e4cef1D618c88d62D2FFA39eC5Eb22D',
+        '0x1854c78e5401A501A8F32f3a9DFae3d356Fb9A9E'
+    ]
+    const topHoldersBalances = await Promise.all(
+        topHolders.map(holder => pact.balanceOf(holder))
+    );
+
     const totalSupply = new BigNumber(100_000_000_000).multipliedBy(decimals);
-    const circulatingSupply = new BigNumber(totalSupply)
-        .minus(airgrabPACTBalance.toString())
-        .minus(daoPACTBalance.toString())
-        .minus(donationMinerPACTBalance.toString())
-        .minus(impactLabsPACTBalance.toString())
-        .minus(idoPACTBalance.toString());
+    let circulatingSupply = new BigNumber(totalSupply)
+    topHoldersBalances.forEach(balance => {
+        circulatingSupply = circulatingSupply.minus(balance.toString());
+    });
+
     const response = circulatingSupply.dividedBy(decimals).toNumber();
 
     // answer
